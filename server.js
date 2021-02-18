4a402dc10dc8db4bea5251e6b1ced60e8e11d704
Cristian Müller <cristian.muller@OSIRIS.local>
2021-02-17 22:10:33 -0300

    adicionado carrinho e loging

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+
+const PORT = process.env.PORT || 3000;
+
+//Assets
+app.use(express.static(path.join(__dirname, '/public')));
+
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+} else {
+  PORT == 3000;
+}
+
+// Setando o template Engine
+
+app.use(expressLayout);
+
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});

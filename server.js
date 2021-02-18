db59ea21a32386c4b9258b5ad8cd9120116f3722
Cristian Müller <cristian.muller@OSIRIS.local>
2021-02-18 11:28:23 -0300

    First Commit

--- server.js
+++ server.js
@@ -22,21 +22,8 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
+require('./routes/web')(app);
 
-app.get('/cart', (req, res) => {
-  res.render('customers/cart');
-});
-
-app.get('/login', (req, res) => {
-  res.render('auth/login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('auth/register');
-});
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
